3c170aadc207c14f14149a9d5fe7085c8a566f5c
Steve De La Rosa <stevedelarosa1036@gmail.com>
2023-09-19 14:35:42 -0400

    updated JS

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ console.log("Hello World!");
 //Set the web browser to current page
 const activePage = window.location.pathname;
 // Get all the anchor tags within the navigation
-const navLinks = document.querySelectorAll("nav a");
+const navLinks = document.querySelector(".nav-links").querySelectorAll("a");
 console.log(navLinks);
 
 // Function to add or remove the 'active-link' class
-navLinks.forEach((link) => {
-    if (link.href.includes(activePage)) {
-        link.classList.add("active");
+navLinks.forEach((el) => {
+    if (el.href.includes(activePage)) {
+        el.classList.add("active");
+    } else {
+        el.classList.remove("active");
     }
 });
