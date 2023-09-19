88ada9e554f1bf12c28a6eaa8aa526ad94e518d5
Steve De La Rosa <stevedelarosa1036@gmail.com>
2023-09-19 00:17:24 -0400

    added JS to underline active link in navbar

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello World!");
+
+//Set the web browser to current page
+const activePage = window.location.pathname;
+// Get all the anchor tags within the navigation
+const navLinks = document.querySelectorAll(".nav-links .nav-item a");
+console.log(navLinks);
+
+// Function to add or remove the 'active-link' class
+navLinks.forEach((link) => {
+    if (link.href.includes(activePage)) {
+        link.classList.add("active");
+    }
+});
